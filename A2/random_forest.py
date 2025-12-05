c3b7cd3e656909a19dddcab77b0847da3ab4ac62
Kartik Arya <82070742+ikc-arya@users.noreply.github.com>
2025-12-05 10:26:10 +0100

    initial random forest structure

---
+++ A2/random_forest.py
@@ -0,0 +1,35 @@
+import regression_tree
+import pandas as pd
+import math
+import random
+from Node import Node
+
+def bootstrap():
+    '''
+    generate the bootstrap sample from original data (with replacement)
+    '''
+    pass
+
+def oob_sample():
+    '''
+    keep track of out of bag samples and compute oob_score
+    '''
+    pass
+
+def prepare_regression_tree():
+    '''
+    creates a regression tree with random features at each node
+    '''
+
+def populate_forest():
+    '''
+    generate regression tree based on a bootstrap sample and polulates the forest
+    '''
+    pass
+
+def predict_from_forest():
+    '''
+    implement majority voting on forest trees to predict
+    '''
+    pass
+
