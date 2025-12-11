d24edff38ea60791d58ff6e7bf8b35171cfef750
Kartik Arya <82070742+ikc-arya@users.noreply.github.com>
2025-12-11 16:20:47 +0100

    Added song dataset, change final_res structure

--- A2/preprocessing/preprocessing_popular_songs.py
+++ A2/preprocessing/preprocessing_popular_songs.py
@@ -1,18 +1,20 @@
 import preprocessing_general as ppg
 import pandas as pd
 
-popular_songs = pd.read_csv("../data/song_data.csv")
-# ppg.df_info(popular_songs)
 
+def preprocessing_polar_songs():
+    popular_songs = pd.read_csv("data/song_data.csv")
+    # ppg.df_info(popular_songs)
 
-print(popular_songs.isnull().any(axis=1).sum())
+    print('Null values count in the dataset',popular_songs.isnull().any(axis=1).sum())
 
-# del popular_songs[popular_songs.columns[0]]
-idx = popular_songs.columns.get_loc("song_name")
-print('Categorical column "song_name" is at index: ',idx)
+    # del popular_songs[popular_songs.columns[0]]
+    idx = popular_songs.columns.get_loc("song_name")
+    print('Categorical column "song_name" is at index: ',idx)
 
-popular_songs_features = ppg.drop_cols(popular_songs, 0)
+    # drop the categorical column
+    popular_songs_features = ppg.drop_cols(popular_songs, 0)
 
+    popular_songs_features_train, popular_songs_features_test  = ppg.train_test_split(popular_songs_features)
 
-popular_songs_features_train, popular_songs_features_test  =ppg.train_test_split(popular_songs_features)
-
+    return popular_songs_features_train, popular_songs_features_test
